293fa5018c8020ae9f3f9592a387e3d50e8d7cce
Maj <majunum@email.com>
2020-07-16 08:30:11 +0530

    1

--- Asset.Model.UserAdmin/Group.cs
+++ Asset.Model.UserAdmin/Group.cs
@@ -6,7 +6,7 @@ namespace Asset.Model.UserAdmin
 {
     public class Group
     {
-        
+        //
         public int? GroupId { get; set; }
         public string GroupName { get; set; }
         public string GroupDescription { get; set; }
